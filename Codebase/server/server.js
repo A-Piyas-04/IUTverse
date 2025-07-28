116e003422893d9ce16967a0eb7194eb1563f0da
NFahmid <nurenfahmid@iut-dhaka.edu>
2025-07-28 17:30:14 +0600

    fixed some bugs

--- Codebase/server/server.js
+++ Codebase/server/server.js
@@ -3,6 +3,16 @@ const config = require('./src/config/config');
 
 const PORT = config.port;
 
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection:', reason);
+});
+
+// Keep the process alive for debugging (remove after testing)
+setInterval(() => {}, 1000);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   console.log('IUTVerse backend ready!');
--- Codebase/server/src/app.js
+++ Codebase/server/src/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
-const { requestLogger } = require('./middleware/logging');
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+const { requestLogger } = require("./middleware/logging");
 
 const app = express();
 
@@ -13,6 +13,6 @@ app.use(cors());
 app.use(requestLogger);
 
 // Routes
-app.use('/', routes);
+app.use("/", routes);
 
 module.exports = app;
